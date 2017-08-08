31a94de930377730ebd85302d3e59bcdfcda2215
Byeonghyeon Lee <inr1319@gmail.com>
2017-08-09 08:59:11 +0900

    added some features on index and added new file main.php

--- index.php
+++ index.php
@@ -2,16 +2,32 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>소망교회에 오신것을 환영합니다</title>
+	<title>소망교회에 오신것을 환영합니다!</title>
 	<style type="text/css">
 		
 	</style>
 	<script type="text/javascript">
-		
+		function guestLogin() {
+			document.getElementById("id").value = "Guest";
+			document.getElementById("password").value = "Guest";
+		}
+
+		function emptyCheck() {
+			if (document.getElementById("id").value == "") {
+				alert("아이디를 입력하십시오!");
+				document.getElementById("id").focus();
+			}
+			else if (document.getElementById("password").value == "") {
+				alert("비밀번호를 입력하십시오");
+				document.getElementById("password").focus();
+			}
+			
+		}
 	</script>
 </head>
 <body>
-	<form accept-charset="utf-8" method="post">
+	<h2>샬롬~ 소망교회에 오신것을 환영합니다</h2>
+	<form accept-charset="utf-8" method="post" action="main.php">
 		<!-- 로그인 정보를 기입하는 부분 -->
 		<div class="login" id="loginForm">
 			<input type="text" name="id" id="id" placeholder="아이디를 입력해주세요" autofocus="autofocus">
@@ -19,8 +35,8 @@
 		</div>
 		<!-- 로그인 정보 입력 후 누를 버튼들이 위치한 부분 -->
 		<div class="buttons" id="buttons">
-			<button class="button" id="signup" type="submit">회원가입</button>
-			<button class="button" id="guestLogin" type="submit" style="padding-left: 5px">방문객입장</button>
+			<button class="button" id="signup" type="submit" formaction="signup.php">회원가입</button>
+			<button class="button" id="guestLogin" type="submit" onclick="guestLogin" style="padding-left: 5px">방문객입장</button>
 			<button class="button" id="login" type="submit" style="padding-left: 5px">로그인</button>
 		</div>
 	</form>
---
+++ main.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>소망교회에 오신것을 환영합니다!</title>
+	<style type="text/css">
+		
+	</style>
+	<script type="text/javascript">
+		
+	</script>
+</head>
+<body>
+	<!-- 로그인에 성공한 경우 이곳에 출력할 안내인사 -->
+	<div>
+		<?php 
+			print $_POSTp['id']. "님 안녕하세요";
+		?>
+	</div>
+	
+</body>
+</html>
