c1a3d9484cd9b6a04fdd3fe2ee8e4303d5e3354a
Byeonghyeon Lee <inr1319@gmail.com>
2017-08-09 09:03:06 +0900

    modified index

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
 </head>
 <body>
 	<h2>샬롬~ 소망교회에 오신것을 환영합니다</h2>
-	<form accept-charset="utf-8" method="post" action="main.php">
+	<form accept-charset="utf-8" method="POST" action="main.php">
 		<!-- 로그인 정보를 기입하는 부분 -->
 		<div class="login" id="loginForm">
 			<input type="text" name="id" id="id" placeholder="아이디를 입력해주세요" autofocus="autofocus">
@@ -36,8 +36,8 @@
 		<!-- 로그인 정보 입력 후 누를 버튼들이 위치한 부분 -->
 		<div class="buttons" id="buttons">
 			<button class="button" id="signup" type="submit" formaction="signup.php">회원가입</button>
-			<button class="button" id="guestLogin" type="submit" onclick="guestLogin" style="padding-left: 5px">방문객입장</button>
-			<button class="button" id="login" type="submit" style="padding-left: 5px">로그인</button>
+			<button class="button" id="guestLogin" type="submit" onclick="guestLogin()" style="padding-left: 5px">방문객입장</button>
+			<button class="button" id="login" type="submit" onclick="emptyCheck()" style="padding-left: 5px">로그인</button>
 		</div>
 	</form>
 	
